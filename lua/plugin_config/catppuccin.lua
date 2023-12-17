f05b98f2bfc818ff4e122480932f6f3c4b2b4121
t-kersting <tke@fedora.fritz.box>
2023-12-17 18:15:34 +0100

    remove catppuccin config

--- lua/plugin_config/catppuccin.lua
+++
@@ -1,7 +0,0 @@
-local mocha = require("catppuccin.palettes").get_palette "mocha"
-
-vim.api.nvim_set_hl(
-    0,
-    "CopilotSuggestion",
-    { italic = true, fg = mocha.overlay0, underline = true }
-)
