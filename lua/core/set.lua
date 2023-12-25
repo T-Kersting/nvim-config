810bab49c6dde54ce8641970da3aecfaf2e49d5b
t-kersting <tke@fedora.fritz.box>
2023-12-25 18:54:46 +0100

    change settings

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -1,7 +1,7 @@
 vim.opt.backspace = '2'
 
 vim.opt.nu = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -17,8 +17,8 @@ vim.opt.breakat = " ^I!@*+;,./?"
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
--- vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
