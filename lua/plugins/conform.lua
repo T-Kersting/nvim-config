35ad0957bd6646eb2267d620577677203da4b2f1
t-kersting <tke@fedora.fritz.box>
2023-12-31 21:36:18 +0100

    use conform for formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "",
+            desc = "[f]ormat buffer with conform"
+        },
+    },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { { "prettierd", "prettier" } },
+        },
+        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    }
+}
