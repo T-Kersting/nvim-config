575e27bd3f2e0f9f187ebf0ec96e265753dd9e17
t-kersting <tke@fedora.fritz.box>
2024-01-01 19:44:54 +0100

    added vim-dadbod

---
+++ lua/plugins/vim-dadbod.lua
@@ -0,0 +1,58 @@
+return {
+    {
+        "tpope/vim-dadbod",
+        opt = true,
+        requires = {
+            "kristijanhusak/vim-dadbod-ui",
+            "kristijanhusak/vim-dadbod-completion",
+        },
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "sql",
+                },
+                command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "sql",
+                    "mysql",
+                    "plsql",
+                },
+                callback = function()
+                    vim.schedule(function()
+                        require("cmp").setup.buffer {
+                            sources = {
+                                { name = "vim-dadbod-completion" }
+                            }
+                        }
+                    end)
+                end,
+            })
+        end,
+    },
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            {
+                'tpope/vim-dadbod',
+                lazy = true
+            },
+            {
+                'kristijanhusak/vim-dadbod-completion',
+                ft = { 'sql', 'mysql', 'plsql' },
+                lazy = true
+            },
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    }
+}
