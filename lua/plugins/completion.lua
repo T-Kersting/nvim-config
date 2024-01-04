ca82686b996c0dde7ef09ba9af056768db06091a
t-kersting <tke@fedora.fritz.box>
2024-01-04 16:58:36 +0100

    added path source to completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -36,6 +36,7 @@ return {
                     sources = cmp.config.sources({
                         { name = 'nvim_lsp' },
                         { name = 'luasnip' },
+                        { name = 'path' },
                     }, {
                         { name = 'buffer' },
                     }),
